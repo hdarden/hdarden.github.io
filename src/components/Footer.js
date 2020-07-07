dc42d1dd0a9abce6225f23f2e54e50f40e6471cc
Helen Darden <hgdarden@gmail.com>
2020-07-07 17:26:59 -0400

    saving

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <footer className="card-footer text-center">
+        &copy; Helen Darden
+      </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header () {
+    return (
+      <header>
+        <div className="container-fluid">
+          <h1 className="display-4 justify-content">Helen Darden</h1>
+          <p className="lead header">
+            Full Stack Web Developer
+          </p>
+        </div>
+      </header>
+    );
+}
+
+export default Header
